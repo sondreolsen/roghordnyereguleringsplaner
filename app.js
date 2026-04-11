d6fa57a171e47bf973aaefc4458ba5c4854fa68f
Sondre Olsen <sondreolsen@gmail.com>
2026-04-11 09:17:31 +0200

    Refine Halhjem ferry terminal position

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ const CURRENT_FERRY_SPECS = [
   {
     id: "halhjem-sandvikvag",
     name: "Halhjem-Sandvikvag",
-    northTerminal: { lon: 5.427726, lat: 60.146198 },
+    northTerminal: { lon: 5.42668, lat: 60.14567 },
     southTerminal: { lon: 5.3381108, lat: 59.9678339 },
     crossingMinutes: 40,
     corridor: {
